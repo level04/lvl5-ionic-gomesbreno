a08002da736f036e4c998b56f033e97b9ef53f93
Breno <breno@debian.Home>
2017-10-31 12:06:03 -0200

    arquivos gerados na primeira aula

---
+++ src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController){
+    
+  }
+
+  ionViewDidLoald(){
+   setTimeout(() => {
+     this.presentToast();
+
+   }, 2000);
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'User was added successfully',
+      duration: 3000
+    });
+    toast.present();
+  }
+
+}
